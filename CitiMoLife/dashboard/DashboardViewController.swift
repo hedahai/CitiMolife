0a8fba228493d246b343796ff6612a6be14e45c1
Tiny <hedahai1985@gmail.com>
2019-02-01 15:50:35 +0800

    update a new controller
    add a new view controller for dsd

---
+++ CitiMoLife/dashboard/DashboardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DashboardViewController.swift
+//  CitiMoLife
+//
+//  Created by 何大海 on 2019/1/31.
+//  Copyright © 2019年 tiny. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
